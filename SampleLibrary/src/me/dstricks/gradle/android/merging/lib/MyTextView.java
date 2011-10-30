2fd7661d56558c5909fbe1364d93a6e0dd0c691b
davids <davids@inqmobile.com>
2011-10-30 21:46:54 +0000

    Added problematic code

---
+++ SampleLibrary/src/me/dstricks/gradle/android/merging/lib/MyTextView.java
@@ -0,0 +1,13 @@
+package me.dstricks.gradle.android.merging.lib;
+
+import android.content.Context;
+import android.widget.TextView;
+
+public class MyTextView extends TextView {
+
+    public MyTextView(Context context) {
+        super(context);
+        // TODO Auto-generated constructor stub
+    }
+
+}
